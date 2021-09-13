d853a84d304b3488a603f6c8894837d73ba01a05
pallavimalge329 <malgepallavi21@gmail.com>
2021-09-13 16:40:00 +0530

    Add project files.

---
+++ LinQ/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+
+namespace LinQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string conn = System.Configuration.ConfigurationManager.ConnectionStrings["LinqToSQLDBConnectionString"].ToString();
+
+            
+
+            string conn = "Data Source = DESKTOP-N5QBMVO\\SQLEXPRESS; Initial Catalog=TQdb; Integrated Security= true";
+            var dc = new EmployeeDataContext(conn);
+            Student newStudent = new Student();
+            newStudent.studentid = 7;
+            newStudent.studentname = "Jyoti";
+            dc.Students.InsertOnSubmit(newStudent);
+            dc.SubmitChanges();
+        }
+    }
+}
